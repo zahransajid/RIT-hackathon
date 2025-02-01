aa7793e79ecaee9dba6f420d7032eae3bbbaca80
Zahran Sajid <99584879+zahransajid@users.noreply.github.com>
2025-02-01 19:02:34 +0800

    Added AWT metric

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ joined_data = pd.read_csv('cache/joined_data.csv')
 days = set(joined_data['day'])
 print(days)
 data_aggregate = {day : {} for day in days}
+x = 0
 
 for day in days:
     day_data = joined_data[joined_data['day'] == day]
@@ -20,14 +21,18 @@ for day in days:
             data_aggregate[day][route] = {"AWT" : [], "kilometers_travelled" : [], "passenger_ridership" : []}
         for date in dates:
             grouped_by_date_and_route = grouped_by_route[grouped_by_route['date'] == date]
+            # if x < 5:
+            #     grouped_by_date_and_route.to_csv(f'cache/{day}_{route}_{date}.csv')
+            #     x += 1
             # Calculate metrics since they are on a per-day per-route basis
-            data_aggregate[day][route]['AWT'].append(calculate_AWT(grouped_by_date_and_route))
+            data_aggregate[day][route]['AWT'].append(calculate_AWT(grouped_by_date_and_route, route, date, day))
             data_aggregate[day][route]['kilometers_travelled'].append(calculate_kilometers_travelled(grouped_by_date_and_route))
             data_aggregate[day][route]['passenger_ridership'].append(calculate_passenger_ridership(grouped_by_date_and_route))
 
-
 # Test code to print metrics for monday on route 96 and E100
 for day,route in zip(['Monday','Monday'],['96','E100']):
-    print(np.average(data_aggregate[day][route]['AWT']))
+    AWT_all = data_aggregate[day][route]['AWT']
+    AWT_avg = np.average([x for x in AWT_all if not np.isnan(x)])
+    print(AWT_avg)
     data_aggregate[day][route]['kilometers_travelled']
     data_aggregate[day][route]['passenger_ridership']
--- src/metrics.py
+++ src/metrics.py
@@ -1,17 +1,49 @@
 import pandas as pd
+import numpy as np
+
 
 # TODO: Add implementations for the following functions
-def calculate_AWT(df : pd.DataFrame) -> float:
-    # takes in a dataframe and returns a float of average waiting time
-    # assume that the df passed is for a specific route on a specific date
-    return 0
+def calculate_AWT(df: pd.DataFrame, route: str, date: str, day: str) -> float:
+    # group by stop and then direction = 0
+    # calculate the average wait time for each stop
+    stops = set(df['stop_code'])
+    stop_WT_direction_0 = []
+    stop_WT_direction_1 = []
 
-def calculate_kilometers_travelled(df : pd.DataFrame) -> float:
+    for stop in stops:
+        stop_data = df[df['stop_code'] == stop]
+        
+        # Direction 0
+        direction_0 = stop_data[stop_data['direction_id'] == 0].sort_values(by=['act_dep_time_x'])
+        if len(direction_0) > 1:
+            WT = direction_0['act_dep_time_x'].diff().dropna().tolist()
+            if(len(WT) == 0):
+                continue
+            stop_WT_direction_0.append(sum(WT) / len(WT))
+
+        # Direction 1
+        direction_1 = stop_data[stop_data['direction_id'] == 1].sort_values(by=['act_dep_time_x'])
+        if len(direction_1) > 1:
+            WT = direction_1['act_dep_time_x'].diff().dropna().tolist()
+            if(len(WT) == 0):
+                continue
+            stop_WT_direction_1.append(sum(WT) / len(WT))
+
+    total_stops = len(stop_WT_direction_0) + len(stop_WT_direction_1)
+    if total_stops == 0:
+        return 0  # Avoid division by zero
+
+    average_WT = (sum(stop_WT_direction_0) + sum(stop_WT_direction_1)) / total_stops
+    return average_WT
+
+
+def calculate_kilometers_travelled(df: pd.DataFrame) -> float:
     # takes in a dataframe and returns the kilometers travelled
     # assume that the df passed is for a specific route on a specific date
     return 0
 
-def calculate_passenger_ridership(df : pd.DataFrame) -> float:
+
+def calculate_passenger_ridership(df: pd.DataFrame) -> float:
     # takes in a dataframe and returns passenger ridership
     # assume that the df passed is for a specific route on a specific date
     return 0
